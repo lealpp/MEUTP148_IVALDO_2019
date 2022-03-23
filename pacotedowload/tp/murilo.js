110ade5c00c727e83d496c0ed190af40c3496e61
Nicolas Leal Almeida <83556859+lealpp@users.noreply.github.com>
2022-03-22 22:50:25 -0300

    commit full tp

---
+++ pacotedowload/tp/murilo.js
@@ -0,0 +1,22 @@
+function validacao(){
+	if (document.form.nome.value=="")
+	{
+		
+		alert("digite o nome")
+		document.form.nome.focus(); 
+		return false;
+	}
+	
+	if (document.form.senha.value.length < 8)
+	{
+		alert("minimo 8 caracter")
+		document.form.senha.focus(); 
+		return false;
+	}
+    if (document.form.email.value == ""  || document.form.email.value.indexOf('@')==-1 || document.form.email.value.indexOf('.')==-1 )
+		{
+			alert("digite um e-mail valido")
+		document.form.email.focus(); 
+		return false;
+		}
+}
